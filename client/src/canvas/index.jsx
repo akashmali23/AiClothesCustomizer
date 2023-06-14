ab0384ff92d928539c9d806e66d1cb13c1672c56
akashmali <devmali247@gmail.com>
2023-06-14 18:25:56 +0530

    updating node issues

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,35 +1,28 @@
-import { Canvas} from '@react-three/fiber'
-import { Environment, Center } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
+
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
 
-
-const CanvasModel  = () => {
+const CanvasModel = () => {
   return (
-    
-      
     <Canvas
-    shadows
-    camera={{position: [0,0,0], fov: 30}}
-    gl={{preserveDrawingBuffer: true}}
-    className="w-full max-w-full h-full transition-all ease-in"
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
     >
-      <ambientLight intensity={0.5}/>
-      <Environment preset="city"/>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
       <CameraRig>
-        <Backdrop/>
-
+        <Backdrop />
         <Center>
-          <Shirt/>
+          <Shirt />
         </Center>
-
       </CameraRig>
     </Canvas>
-      
-      
-     
   )
 }
 
